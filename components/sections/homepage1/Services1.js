d39a73fdaba719264ce000e9363736adce0ebb64
nayabgull067 <nayabkhankhan918@gmail.com>
2025-01-23 09:26:14 -0800

    Updated changes

--- components/sections/homepage1/Services1.js
+++ components/sections/homepage1/Services1.js
@@ -304,7 +304,7 @@ landfills. With trusted relationship with local landfills, we work hard to recyc
                                     <div className="card-image"><img src="/assets/imgs/page/homepage1/train.png" alt="transp" />
                                     </div>
                                     <div className="card-info">
-                                        <h5 className="color- mb-15"style={{color:'black'}}>Free quotes & you'll never have to someone, When Booking Online</h5>
+                                        <h5 className="color- mb-15"style={{color:'black'}}>Free quotes & you'll never have to someone, we Booking Online</h5>
                                         <p className="font-sm color-grey-900 mb-35"></p>
                                         <div className="box-button-offer mb-30"style={{marginTop:'80px'}}><a className="btn btn-link font-sm color-brand-2">View Details<span>
                                             <svg className="w-6 h-6 icon-16" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
